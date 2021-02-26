f01d864bc3e3b852817d2d4ad25caa911d5631ec
Mayank kumar <Mayankk104@gmail.com>
2021-02-26 12:28:34 +0530

    Pangram

---
+++ Practices/Strings/Pangram.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<conio.h>
+#include<vector>
+
+using namespace std;
+
+bool is_pangram(string& str);
+
+int main(){
+    string str;
+
+    cout<<"Enter a  sentance: ";
+    getline(cin,str);
+   
+    bool prangram = is_pangram(str);
+
+    if(prangram == true){
+        cout<<"YES, Given sentence is a Pangram.";
+    }else{
+        cout<<"NO, Given sentence is not a Pangram.";
+    }
+    
+    getch();
+}
+
+
+//================
+//    DEFINATION
+//================
+bool is_pangram(string& str){
+    vector<int> v(26,false);
+    int index;
+
+    for(int i=0;i<str.length();i++){
+        if(str[i]>='A' && str[i]<='Z'){
+            index = str[i]-'A';
+        }
+        else if(str[i]>='a' && str[i]<='z'){
+            index = str[i]-'a';
+        }else{
+            continue;
+        }
+
+        v[index] = true;
+    }
+
+    for(int i=0;i<26;i++){
+        if(v[i]== false){
+            return false;
+        }
+    }
+
+    return true;
+}
