9619c2657d0b3956c9793bd7ea822cc0a4453c5a
Max <Mayankk1042017@outlook.com>
2022-03-05 13:40:14 +0530

    @sorting/bubble

---
+++ Practices/Sortings/Bubble.ts
@@ -0,0 +1,20 @@
+function bubble(arr: number[]): number[]{
+    let swap:boolean;
+    let l = arr.length-1;
+
+    for(let i=0;i<l;i++){
+        swap =false;
+        for(let j=0; j<l-i; j++){
+            if(arr[j]>arr[j+1]){
+                swap =true;
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]]; 
+            }
+        }
+        if(!swap)
+            break;
+    }
+
+    return arr;
+}
+
+console.log(bubble([1, 5, 7, 10, 12, 14, 15, 18, 20,22, 25, 27, 300, 64, 110, 220]));
