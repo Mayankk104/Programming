a4a71f691a000438eb25ff403250f5090b56f960
Mayank kumar <Mayankk104@gmail.com>
2022-03-12 15:23:24 +0530

    Insertion Sort

--- Practices/Sortings/Insertion.ts
+++ Practices/Sortings/Insertion.ts
@@ -1,17 +1,32 @@
-console.clear();
+let arr: number[] = [];
+let randomNumber: number;
 
-// TODO: Complete insertion sort.
 function Insertion(arr: number[]) {
-    for (let i = 1; i < arr.length; i++) {
-        for (let j = i - 1; j >= 0; j--)
-            if (arr[i] < arr[j]) {
-                arr.splice(j - 1, 0, arr[i]);
+    for (let i = 1; i < arr.length; i++)
+        for (let j = i - 1; j >= -1; j--)
+            if (arr[i] > arr[j] || j === -1) {
+                arr.splice(j + 1, 0, arr[i]);
                 arr.splice(i + 1, 1);
                 break;
             }
-    }
+
 
     return arr;
 }
 
-console.log(Insertion([1, 0]));
+for (let i = 0; i < 100; i++) {
+    randomNumber = Math.floor(Math.random() * 200);
+    arr.push(randomNumber);
+}
+
+console.log(arr);
+console.log(Insertion(arr));
+
+
+// Space Complexity
+    // O(1)
+
+// Time Complexity
+    // Best   O(n)
+    // Avg    O(n²)
+    // Worst  O(n²)
