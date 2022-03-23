a364a29a3af8a7168bed59330b56eb6a64d913ad
Mayank kumar <Mayankk104@gmail.com>
2022-03-23 15:55:23 +0530

    binary search

---
+++ Practices/Array/binary_search.ts
@@ -0,0 +1,19 @@
+function search(nums: number[], target: number): number {
+    let l = -1, r = nums.length;
+    let m: number;
+
+    while (r - l > 1) {
+        m = Math.floor((r - l) / 2) + l;
+
+        if (nums[m] === target)
+            return m;
+
+        if (nums[m] > target)
+            r = m;
+        else
+            l = m;
+
+    }
+
+    return -1;
+};
