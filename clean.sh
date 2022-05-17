c3e8979e1c958cca9bf271e9b0bfe1ecd2b46674
Mayank <mayankk104@gmail.com>
2022-05-17 18:54:36 +0530

    bash file for cleaning workspace

---
+++ clean.sh
@@ -0,0 +1,9 @@
+
+find src/ -type f ! -name "*.*"
+find src/ -type f ! -name "*.*" -exec rm {} \;
+
+find -E src/ -regex '.*\.(o|out|exe)' 
+find -E src/ -regex '.*\.(o|out|exe)' -delete
+
+find src/ -type d -name "*.dSYM*"
+find src/ -type d -name "*.dSYM*" -exec rm -r {} +
