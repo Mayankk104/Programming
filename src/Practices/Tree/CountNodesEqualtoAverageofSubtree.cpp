8b618aee2872e5988b4c345c49ccd7947204a140
Mayank <mayankk104@gmail.com>
2022-05-27 13:51:19 +0530

    improving: Count Nodes Equal to Average of Subtree

--- src/Practices/Tree/CountNodesEqualtoAverageofSubtree.cpp
+++ src/Practices/Tree/CountNodesEqualtoAverageofSubtree.cpp
@@ -15,35 +15,49 @@ struct TreeNode
 class Solution
 {
 public:
-    int count;
-
-    pair<int, int> helper(TreeNode *node)
+    pair<int, int> helper(TreeNode *node, int &count)
     {
         pair<int, int> p = {node->val, 1};
 
         if (node->right != NULL)
         {
-            pair<int, int> tempP = helper(node->right);
+            pair<int, int> tempP = helper(node->right, count);
             p.first += tempP.first;
             p.second += tempP.second;
         }
 
         if (node->left != NULL)
         {
-            pair tempP = helper(node->left);
+            auto tempP = helper(node->left, count);
             p.first += tempP.first;
             p.second += tempP.second;
         }
 
         if (node->val == (p.first / p.second))
-            this->count++;
+            count++;
 
         return p;
     }
 
     int averageOfSubtree(TreeNode *root)
     {
-        helper(root);
+        int count = 0;
+        helper(root, count);
         return count;
     }
 };
+
+int main()
+{
+    Solution s;
+
+    TreeNode n(4);
+    TreeNode n2(10);
+    TreeNode n3(100);
+    TreeNode *ptr = &n;
+
+    n.left = &n2;
+    n.right = &n3;
+
+    cout << s.averageOfSubtree(ptr);
+}
