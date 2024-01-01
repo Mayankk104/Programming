3232c2020d7254cc4a2f51f543a9db73d9fc4cd6
Mayank kumar <mayankk104@gmail.com>
2024-01-01 22:34:39 +0530

    maximum contiguous sum

---
+++ src/Array n String/Array/max_sum_contiguous_subarray.cpp
@@ -0,0 +1,28 @@
+// Tags: #easy #scaler #M4DSA1 #array1 #assingment #kadane's algorithm
+// Time Complexity: O(n) | Space Complexity: O(1)
+
+#include <climits>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    int max_sum(const vector<int>& arr) {
+        int max_sum = INT_MIN, sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+            max_sum = max(sum, max_sum);
+            if (sum < 0) sum = 0;
+        }
+
+        return max_sum;
+    }
+};
+
+int main() {
+    Solution solution;
+    cout << "maximun contiguous sum: " << solution.max_sum({-2, 1, -3, 4, -1, 2, 1, -5, 4}) << endl;
+}
