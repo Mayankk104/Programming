2de781820e13c7730a799ef51766a073b317b1be
Mayank kumar <mayankk104@gmail.com>
2024-02-26 22:21:12 +0530

    minimum jumps improvements

--- src/Array n String/Array/minimum_jumps.cpp
+++ src/Array n String/Array/minimum_jumps.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
 #include <vector>
-// TODO: complete it
+
 using namespace std;
+
+// NOTE: almost perfect answer passed on leetcode but not on GFG and
 
 class Solution {
    public:
     int minimum_jumps(vector<int> arr) {
-        int position = 0, jumps = 0;
+        int position = 0, furthest = 0, furthest_index = 0, jumps = 0;
 
         while (position < arr.size()) {
-            int furthest = position;
-            // cout << position << " " << furthest << endl;
-
-            for (int i = position; i < position + arr[position]; i++) {
-                cout << position + arr[i] << " ";
-                if (position + arr[i] > arr.size()) return jumps++;
-                if ((arr[i] + position) > furthest) furthest = arr[i] + position;
+            if (arr[position] + position >= arr.size() - 1) return ++jumps;
+            for (int i = position + 1; i <= arr[position] + position; i++) {
+                if (arr[i] + i > furthest) {
+                    furthest_index = i;
+                    furthest = arr[i] + i;
+                }
+                if (arr[i] + i >= arr.size() - 1) return jumps + 2;
             }
-            cout << endl;
-
-            position = furthest;
+            position = furthest_index;
+            if (arr[furthest_index] == 0) return -1;
             jumps++;
         }
-        return 0;
+
+        return jumps;
     }
 };
 
 int main() {
-    vector<int> input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    vector<int> input = {3, 6, 6, 10, 6, 1, 3, 1, 10, 1, 1, 10, 1, 7, 7, 2, 3, 1, 2, 4, 5, 8, 7, 2, 6, 8, 6, 7, 5, 4, 10, 4, 8, 10, 8};
     Solution solution;
 
-    cout << solution.minimum_jumps(input);
+    int jumps = solution.minimum_jumps(input);
+
+    cout << "minimum jumps: " << jumps;
 }
