70a8391725b01997a1d4845367b6df56403e9dc3
Mayank kumar <mayankk104@gmail.com>
2024-03-30 12:44:27 +0530

    merge intervals

--- src/Array n String/Array/merge_intervals.cpp
+++ src/Array n String/Array/merge_intervals.cpp
@@ -1,26 +1,26 @@
+// Merge Intervals: https://www.interviewbit.com/problems/merge-intervals/
+// Tags: #medium #scaler #M4DSA1 #array2 #assignment
+// Efficiency: Time Complexity O(N logN), Space Complexity O(N)
+
+#include <algorithm>
 #include <iostream>
 #include <vector>
-// TODO: Try Again
+
 using namespace std;
 
 class Solution {
    public:
     vector<vector<int>> merge(vector<vector<int>> &intervals, vector<int> new_interval) {
+        intervals.push_back(new_interval);
+        sort(intervals.begin(), intervals.end());
+
         vector<vector<int>> result = {intervals[0]};
 
-        for (int i = 1; i < intervals.size(); i++) {
-            if (result[result.size() - 1][1] < new_interval[0]) {
-                result[result.size() - 1][0] = min(result[result.size() - 1][0], new_interval[0]);
-                result[result.size() - 1][1] = max(result[result.size() - 1][1], new_interval[1]);
-            }
-
-            if (result[result.size() - 1][1] < intervals[i][0]) {
-                result[result.size() - 1][0] = min(result[result.size() - 1][0], intervals[i][0]);
-                result[result.size() - 1][1] = max(result[result.size() - 1][1], intervals[i][1]);
-            } else {
+        for (int i = 1; i < intervals.size(); i++)
+            if (intervals[i][0] > result[result.size() - 1][1])
                 result.push_back(intervals[i]);
-            }
-        }
+            else
+                result[result.size() - 1][1] = max(result[result.size() - 1][1], intervals[i][1]);
 
         return result;
     }
@@ -31,8 +31,8 @@ int main() {
     vector<vector<int>> input = {{1, 3}, {6, 9}};
     vector<int> new_interval = {2, 5};
 
-    vector<vector<int>> result = solution.merge(input, new_interval);
+    vector<vector<int>> output = solution.merge(input, new_interval);
 
-    for (vector<int> interval : result)
+    for (vector<int> interval : output)
         cout << interval[0] << " " << interval[1] << " -> ";
 }
