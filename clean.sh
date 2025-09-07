99e005b17dbff1023188749c2cb0968d42a50baa
Mayank kumar <mayankk104@gmail.com>
2025-09-07 14:31:49 +0530

    removing c++ and ts specific files
    from this points on words this will be starting point of workspaces (go,
    java, c++ etc.) to be created from here

--- clean.sh
+++
@@ -1,8 +0,0 @@
-find src/ -type f ! -name "*.*"
-find src/ -type f ! -name "*.*" -exec rm {} \;
-
-find -E src/ -regex '.*\.(o|out|exe)' 
-find -E src/ -regex '.*\.(o|out|exe)' -delete
-
-find src/ -type d -name "*.dSYM*"
-find src/ -type d -name "*.dSYM*" -exec rm -r {} +
